d2074bb30804ede5da4492d38cd011ae0e2fedd8
Nikhil J K <nikhil.jk02@gmail.com>
2024-09-25 21:28:23 -0600

    Delete Crime-data-analysis directory

--- Crime-data-analysis/js/script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-
-// Function to show alert when button is clicked
-function showAlert() {
-    alert('This is a simple alert triggered by a button!');
-}
-
-// You can also attach functions to buttons directly in the HTML file
